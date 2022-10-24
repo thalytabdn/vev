39d6e29dd46ea52c126f35a2846b46220d8170e9
thalytabdn <thalyta.nascimento@ccc.ufcg.edu.br>
2022-10-24 17:08:33 -0300

    add teste calcula salario desenvolvedor salario menor que 3000

--- Exercício 2 - Praticando TDD/CalculadoraDeSalario/src/tests/CalculadoraTets.java
+++ Exercício 2 - Praticando TDD/CalculadoraDeSalario/src/tests/CalculadoraTets.java
@@ -10,24 +10,32 @@ import calculadora.Cargo;
 import calculadora.Funcionario;
 
 public class CalculadoraTets {
-  Funcionario funcionario = new Funcionario("12345685412", "Thalyta Nascimento", "thalyta@gmail.com", 6520.80, Cargo.DESENVOLVEDOR);
+  Funcionario funcionario1 = new Funcionario("12345685412", "Desenvolvedor 1", "dev1@gmail.com", 6520.80, Cargo.DESENVOLVEDOR);
+
+  Funcionario funcionario2 = new Funcionario("12345685412", "Desenvolvedor 2", "dev2@gmail.com", 2530.80, Cargo.DESENVOLVEDOR);
+  
   Calculadora calculadora = new Calculadora();
   
   @Test
   public void criaFuncionarioTest() {
-    assertEquals(this.funcionario.getId(), "12345685412");
-    assertEquals(this.funcionario.getNome(), "Thalyta Nascimento");
-    assertEquals(this.funcionario.getEmail(), "thalyta@gmail.com");
-    assertTrue(this.funcionario.getSalario() == 6520.80);
-    assertEquals(this.funcionario.getCargo(), Cargo.DESENVOLVEDOR);
+    assertEquals(this.funcionario1.getId(), "12345685412");
+    assertEquals(this.funcionario1.getNome(), "Thalyta Nascimento");
+    assertEquals(this.funcionario1.getEmail(), "thalyta@gmail.com");
+    assertTrue(this.funcionario1.getSalario() == 6520.80);
+    assertEquals(this.funcionario1.getCargo(), Cargo.DESENVOLVEDOR);
   }
 
   @Test
   public void calculaSalarioDesenvolvedorTest(){
-    Cargo cargo = this.funcionario.getCargo();
-    double salario = this.funcionario.getSalario();
+    Cargo cargo1 = this.funcionario1.getCargo();
+    double salario1 = this.funcionario1.getSalario();
 
-    assertTrue(this.calculadora.calculaSalario(cargo, salario) == (salario - (salario * 0.20))) ;    
+    assertEquals(this.calculadora.calculaSalario(cargo1, salario1), (salario1 - (salario1 * 0.20)), 0) ;  
+
+    Cargo cargo2 = this.funcionario2.getCargo();
+    double salario2 = this.funcionario2.getSalario();
+
+    assertEquals(this.calculadora.calculaSalario(cargo2, salario2),salario2 - (salario2 * 0.10), 0 );
 
   }
   
