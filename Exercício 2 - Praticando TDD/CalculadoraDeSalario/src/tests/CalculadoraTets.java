11bda9052ab20ca3f2c3bf51f41c3f2f2cd3d3e5
thalytabdn <thalyta.nascimento@ccc.ufcg.edu.br>
2022-10-24 16:24:51 -0300

    add test calcula salario

--- Exercício 2 - Praticando TDD/CalculadoraDeSalario/src/tests/CalculadoraTets.java
+++ Exercício 2 - Praticando TDD/CalculadoraDeSalario/src/tests/CalculadoraTets.java
@@ -9,15 +9,25 @@ import calculadora.Cargo;
 import calculadora.Funcionario;
 
 public class CalculadoraTets {
-
+  Funcionario funcionario = new Funcionario("12345685412", "Thalyta Nascimento", "thalyta@gmail.com", 6520.80, Cargo.DESENVOLVEDOR);
+  Calculadora calculadora = new Calculadora();
+  
   @Test
   public void criaFuncionarioTest() {
-    Funcionario funcionario = new Funcionario("12345685412", "Thalyta Nascimento", "thalyta@gmail.com", 6520.80, Cargo.DESENVOLVEDOR);
-    assertEquals(funcionario.getId(), "12345685412");
-    assertEquals(funcionario.getNome(), "Thalyta Nascimento");
-    assertEquals(funcionario.getEmail(), "thalyta@gmail.com");
-    assertTrue(funcionario.getSalario() == 6520.80);
-    assertEquals(funcionario.getCargo(), Cargo.DESENVOLVEDOR);
+    assertEquals(this.funcionario.getId(), "12345685412");
+    assertEquals(this.funcionario.getNome(), "Thalyta Nascimento");
+    assertEquals(this.funcionario.getEmail(), "thalyta@gmail.com");
+    assertTrue(this.funcionario.getSalario() == 6520.80);
+    assertEquals(this.funcionario.getCargo(), Cargo.DESENVOLVEDOR);
+  }
+
+  @Test
+  public void calculaSalarioTest(){
+    Cargo cargo = this.funcionario.getCargo();
+    double salario = this.funcionario.getSalario();
+
+    assertEquals(this.calculadora.calculaSalarioTest(cargo, salario), (salario - (salario * 0.20))) ;
+    
   }
   
 }
