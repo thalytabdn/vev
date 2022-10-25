9cd27586ac9731ee8deb410319e92775fd158dcd
thalytabdn <thalyta.nascimento@ccc.ufcg.edu.br>
2022-10-24 22:07:45 -0300

    add metodo para verificar se usuário ja existe

--- Exercício 2 - Praticando TDD/CalculadoraDeSalario/src/calculadora/App.java
+++ Exercício 2 - Praticando TDD/CalculadoraDeSalario/src/calculadora/App.java
@@ -1,6 +1,72 @@
 package calculadora;
+
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Calculadora calculadora = new Calculadora();
+
+        while (true) {
+
+            System.out.println("(A)dicionar Funcionario");
+            System.out.println("(C)alcular Salário");
+            System.out.println("(S)air");
+            System.out.println();
+
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Opção> ");
+            String opcao = sc.nextLine().toLowerCase();
+
+            if (opcao.equals("a")) {
+                addFuncionario(calculadora);
+            } else if (opcao.equals("c")) {
+                calculaSalario(calculadora);
+            } else if (opcao.equals("s")) {
+                break;
+            } else {
+                System.out.println("OPÇÃO INVALIDA!");
+            }
+        }
+
+    }
+
+    private static void calculaSalario(Calculadora calculadora) {
+        Scanner sc = new Scanner(System.in);
+		System.out.print("Posição: ");
+		int pos = sc.nextInt();
+		sc.nextLine();
+		if (true) {
+			System.out.println("POSIÇÃO INVÁLIDA!");
+
+		} else {}
+    }
+
+    private static void addFuncionario(Calculadora calculadora) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("CPF: ");
+        String cpf = sc.nextLine();
+
+        System.out.print("Nome: ");
+        String nome = sc.nextLine();
+
+        System.out.print("Email: ");
+        String email = sc.nextLine();
+
+        System.out.print("Salário: ");
+        String salario = sc.nextLine();
+
+        System.out.print("Cargo: ");
+        String cargo = sc.nextLine();
+
+        try {
+            calculadora.addFuncionario(cpf, nome, email, Double.parseDouble(salario),
+                    Cargo.valueOf(cargo.toUpperCase()));
+            System.out.println("FUNCIONÁRIO CADASTRADO !");
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+        }
+
+        System.out.println("");
     }
 }
--- Exercício 2 - Praticando TDD/CalculadoraDeSalario/src/calculadora/Calculadora.java
+++ Exercício 2 - Praticando TDD/CalculadoraDeSalario/src/calculadora/Calculadora.java
@@ -1,25 +1,43 @@
 package calculadora;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Calculadora {
-  private ArrayList<Funcionario> funcionarios;
+  private HashMap<String, Funcionario> funcionarios;
 
   public Calculadora() {
-    this.funcionarios = new ArrayList<>();
+    this.funcionarios = new HashMap<>();
   }
 
   public void addFuncionario(String id, String nome, String email, double salario,  Cargo cargo){
     Funcionario funcionario = new Funcionario(id, nome, email, salario, cargo);
-    this.funcionarios.add(funcionario);
+    this.funcionarios.put(id, funcionario);
+  }
+
+  public boolean funcionarioExiste(String id){
+    return this.funcionarios.containsKey(id);
+  }
+
+  public Funcionario getFuncionario(String id){
+    if(this.funcionarios.containsKey(id)){
+      return this.funcionarios.get(id);
+    }else{
+      throw new NullPointerException("Funcionário não cadastrado.");
+    }
   }
 
   public int getQtdFuncionarios(){
     return this.funcionarios.size();
   }
 
-  public double calculaSalario(Cargo cargo, double salario){
+  public double calculaSalario(String id){
+    Funcionario funcionario = getFuncionario(id);
+    Cargo cargo = funcionario.getCargo();
+    double salario = funcionario.getSalario();
+
     double salarioLiquido = 0;
+    
     if(cargo.equals(Cargo.DESENVOLVEDOR) && salario >= 3000){
       salarioLiquido = salario - (salario * 0.20);
     }
