c2bcf6c767a6bf334756b76bbb2ffbe42011923a
JGuilhermeCoelho <jose.guilherme.oliveira@ccc.ufcg.edu.br>
2022-10-25 00:57:52 -0300

    Cria teste cliente

---
+++ Exercício 2 - Praticando TDD/FiltroDeFaturas/src/test/java/filtro/ClienteTest.java
@@ -0,0 +1,20 @@
+package filtro;
+
+import fatura.Cliente;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Date;
+
+public class ClienteTest {
+
+    @Test
+    public void teste() {
+        Cliente cliente = new Cliente("Guilherme Coelho", new Date(2022, 11, 30), "PE");
+
+        Assert.assertEquals("Guilherme Coelho", cliente.getNome());
+        Assert.assertEquals(new Date(2022, 11, 30), cliente.getData());
+    }
+
+
+}
