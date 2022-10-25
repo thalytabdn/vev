fe2e936169a5fc72ce3ac57132349455150ef8c0
thalytabdn <thalyta.nascimento@ccc.ufcg.edu.br>
2022-10-24 21:18:56 -0300

    add funcionario em calculadora

--- Exercício 2 - Praticando TDD/CalculadoraDeSalario/src/calculadora/Calculadora.java
+++ Exercício 2 - Praticando TDD/CalculadoraDeSalario/src/calculadora/Calculadora.java
@@ -1,6 +1,22 @@
 package calculadora;
 
+import java.util.ArrayList;
+
 public class Calculadora {
+  private ArrayList<Funcionario> funcionarios;
+
+  public Calculadora() {
+    this.funcionarios = new ArrayList<>();
+  }
+
+  public void addFuncionario(String id, String nome, String email, double salario,  Cargo cargo){
+    Funcionario funcionario = new Funcionario(id, nome, email, salario, cargo);
+    this.funcionarios.add(funcionario);
+  }
+
+  public int getQtdFuncionarios(){
+    return this.funcionarios.size();
+  }
 
   public double calculaSalario(Cargo cargo, double salario){
     double salarioLiquido = 0;
